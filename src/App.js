5a0608442aa2c12e8a491545a93398c1822d1524
Kasia_Ngk <ngk.kasia@gmail.com~>
2024-02-19 12:07:38 +0100

    creation State - money, basket's array and products's array

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+ 
+  const [money, setMoney] = useState(20)
+  const [basket, setBasket] = useState([])
+  const [products] = useState([
+    { nom: 'Product 1', price: 5, stock: 5, img: '' },
+    { nom: 'Product 2', price: 8, stock: 3, img: '' },
+    { nom: 'Product 3', price: 10, stock: 0, img: '' }
+  ]);
+
+
   return (
-    <div className="App">
-      <h1>YO</h1>
+    <div className='OG'>
+      <h1>Mon eShop</h1>
     </div>
   );
-}
+};
 
 export default App;
